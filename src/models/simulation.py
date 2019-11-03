920060628d60a4dde0ffaf82ed3cf18151bd4e6b
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2019-11-03 21:51:10 +0100

    feat: add cli runtime info model. add serializer to models

--- src/models/simulation.py
+++ src/models/simulation.py
@@ -30,6 +30,7 @@ class Simulation(Base):
 
 class SimulationStep(Base):
     __tablename__ = 'steps'
+    RELATIONSHIPS_TO_DICT = True
 
     id = Column(Integer, primary_key=True)
     simulation_id = Column(Integer, ForeignKey('simulations.id'))
@@ -37,19 +38,35 @@ class SimulationStep(Base):
     finished_utc = Column(DateTime)
     origin = Column(Enum(Step))
     celery_id = Column(String())
+    cli_runtime_info = relationship("CliRuntimeInfo", uselist=False, backref="steps")
     artifacts = relationship("Artifact", backref="steps")
-    simulation = relationship("Simulation", back_populates="steps")
+
+    def __json__(self):
+        return ['id', 'simulation_id', 'started_utc', 'finished_utc', 'origin', 'artifacts', 'cli_runtime_info']
 
 
 class Artifact(Base):
     __tablename__ = 'artifacts'
 
     id = Column(Integer, primary_key=True)
-    simulation_id = Column(Integer, ForeignKey('simulations.id'))
     step_id = Column(Integer, ForeignKey('steps.id'))
+    simulation_id = Column(Integer, ForeignKey('simulations.id'))
     created_utc = Column(DateTime)
     size_kb = Column(Integer)
     path = Column(String())
-    simulation = relationship("Simulation", back_populates="artifacts")
-    step = relationship("SimulationStep", back_populates="artifacts")
+
+    def __json__(self):
+        return ['id', 'created_utc', 'size_kb', 'path']
+
+
+class CliRuntimeInfo(Base):
+    __tablename__ = 'cli_runtime_infos'
+
+    id = Column(Integer, primary_key=True)
+    step_id = Column(Integer, ForeignKey('steps.id'))
+    cpu = Column(Integer)
+    memory = Column(Integer)
+
+    def __json__(self):
+        return ['cpu', 'memory']
 
