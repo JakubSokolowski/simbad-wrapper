73ac88b2d9d992da74208785482324c00ca40581
Jakub Sokolowski <jakub.sokolowski1996@gmail.com>
2019-11-24 08:30:27 +0100

    feat: add endpoints for artifact info and downloading artifacts. add .zip compression utils for artifact files and folders

--- src/server/artifacts/api.py
+++ src/server/artifacts/api.py
@@ -1,10 +1,25 @@
 import logging
-from flask import Blueprint, send_file
+
+from flask import Blueprint, send_file, jsonify
+
+from database import db_session
+from models.simulation import Artifact
+from server.artifacts.utils import compress_artifact
 
 artifact_api = Blueprint('artifact', __name__)
 logger = logging.getLogger()
 
 
-@artifact_api.route('/download/<path>', methods=['GET'])
-def download(path: str):
-    return send_file(path)
+@artifact_api.route('/<artifact_id>', methods=['GET'])
+def get_info(artifact_id: int):
+    artifact: Artifact = db_session.query(Artifact).get(artifact_id)
+    if artifact is not None:
+        return jsonify(artifact)
+    return 404
+
+
+@artifact_api.route('/<artifact_id>/download', methods=['GET'])
+def download(artifact_id: int):
+    artifact: Artifact = db_session.query(Artifact).get(artifact_id)
+    archive_path = compress_artifact(artifact.path)
+    return send_file(archive_path)
---
+++ src/server/artifacts/utils.py
@@ -0,0 +1,39 @@
+import ntpath
+import os
+import shutil
+
+
+def path_leaf(path: str) -> str:
+    """
+    Returns the leaf of the path:
+    /home/some/path -> path
+    /home/some/path/file.csv -> file.csv
+    :param path:
+    :return:
+    """
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+
+def compress_artifact(path: str) -> str:
+    """
+    Compresses artifact (or folder in case of .parquet artifacts) to zip format
+    :param path: path to artifact or folder with artifacts
+    :return: path to created zip file
+    """
+    if os.path.isdir(path):
+        artifact_dir: str = path
+        parent_dir: str = os.path.dirname(path)
+        archive_name: str = path_leaf(path)
+        archive_path: str = "{}/{}".format(parent_dir, archive_name)
+        shutil.make_archive(archive_path, 'zip', artifact_dir)
+        return "{}/{}".format(parent_dir, archive_name + '.zip')
+    # TODO Quick and dirty method -> refactor to avoid code duplication
+    else:
+        parent_dir: str = os.path.dirname(path)
+        archive_name: str = path_leaf(path)
+        archive_path = "{}/{}".format(parent_dir, archive_name)
+        shutil.make_archive(archive_path, 'zip', parent_dir, path_leaf(path))
+        return "{}/{}".format(parent_dir, archive_name + '.zip')
+
+
